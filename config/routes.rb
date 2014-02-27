b3135fd26b9a7816901ab100052431abcc47629e
Adam Andrewjeski <aandrewjeski@gmail.com>
2014-02-26 22:56:23 -0800

    added Doge

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 AboutMe::Application.routes.draw do
   root "pages#home"
   get "about" =>  "pages#about"
+  get "doge" => "pages#doge"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
